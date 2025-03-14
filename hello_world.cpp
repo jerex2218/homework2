57729f057d572eb98ce3a73f787b4dbfdcfa9d5f
jerex2218 <tprivet92@gmail.com>
2025-03-14 16:02:18 +0000

    Update hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 using namespace std;
 int main() {
-	cout << "Hello, World!" << endl;
+	string name;
+	cout << "Enter your name, please:
+	cin >> name;
+	cout << "Hello world from " << name << endl;
 	return 0;
 }
